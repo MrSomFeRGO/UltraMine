2d1a6ca2ca98c252358dc574ed2c6f84a8a2a8b8
MrSomFeRGO <mr.somfergo@gmail.com>
2023-09-15 00:56:34 +0300

    update libraries and ultramine_core

--- ultramine/libraries/Start.cmd
+++ ultramine/libraries/Start.cmd
@@ -1,4 +1,4 @@
 chcp 1251
-java -server -XX:+UseG1GC -XX:+UseStringDeduplication -Xms2G -Xmx4G -jar ultramine_core-1.7.10-server-0.1.5.jar
+java -server -XX:+UseG1GC -XX:+UseStringDeduplication -Xms2G -Xmx4G -jar ultramine_core-1.7.10-server-0.1.6.jar
 chcp 866
 pause
